c3e406064cc111b2e275a1df8c64899e500f4836
kedior <kedior123@gmail.com>
2025-07-18 17:27:48 +0000

    auto deploy from private repo

---
+++ dist/chunk-gv17y4v7.js
@@ -0,0 +1,2 @@
+import{q as a}from"./chunk-w84etn2h.js";function n(){return a("div",{style:"text-align: center",children:[a("h1",{children:"Oops! This page galloped away on a unicorn."},void 0,!1,void 0,this),a("img",{src:"./unicorn.png",alt:"Lost Unicorn",style:"max-width: 300px; margin: 40px auto; display: block"},void 0,!1,void 0,this)]},void 0,!0,void 0,this)}
+export{n as a};
