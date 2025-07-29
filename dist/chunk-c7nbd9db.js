92b609496ec5d7fe77132d75229afb8d50d89cc0
kedior <kedior123@gmail.com>
2025-07-29 13:50:26 +0000

    auto deploy from private repo

--- dist/chunk-c7nbd9db.js
+++
@@ -1,2 +0,0 @@
-import{t as a,u as n}from"./chunk-sn5a9rvh.js";var l=()=>{return n("div",{style:{textAlign:"center"},children:[a("h1",{children:"Oops! This page galloped away on a unicorn."}),a("img",{src:"./unicorn.png",alt:"Lost Unicorn",style:{maxWidth:"300px",margin:"40px auto",display:"block"}})]})},t=l;
-export{t as a};
