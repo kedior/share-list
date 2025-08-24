ecfd066e16ac3df73a0e3ff3b0a1a566b641c885
kedior <kedior123@gmail.com>
2025-08-24 11:52:15 +0000

    auto deploy from private repo

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "bun & gleam env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ bun gleam ];
+          shellHook = ''
+            export PS1="(nix-dev) $PS1"
+            echo "Bun version: $(bun --version)"
+            echo "Gleam version: $(gleam --version)"
+            bun install
+            gleam deps download
+          '';
+        };
+      });
+}
+
