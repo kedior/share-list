2c3f371f22045771c0e3db4fe41afebfcc5754db
kedior <kedior123@gmail.com>
2025-09-12 19:00:55 +0000

    auto deploy from private repo

--- build.ts
+++ build.ts
@@ -5,18 +5,22 @@ const distDir = "./out";
 await rm(distDir, { recursive: true, force: true });
 await mkdir(distDir, { recursive: true });
 
-const entryJsPath = "./out/app.js";
-const entryHtmlPath = "./out/index.html";
+const entryJsPath = "./out/__entry.js";
 const jsCode = `import { main } from "../build/dev/javascript/app/app.mjs";main();`;
-const htmlCode = `<!doctype html><html lang="en"><script type="module" src="./app.js"></script></html>`;
 await writeFile(entryJsPath, jsCode);
-await writeFile(entryHtmlPath, htmlCode);
-
 const result = await build({
   entrypoints: [entryJsPath],
-  outdir: distDir,
   target: "browser",
   minify: true,
 });
 
+let bundledJs = await result.outputs[0]?.text()!
 console.log(result);
+await rm(entryJsPath);
+
+const safeJs = bundledJs.replace(/<\/script>/g, "<\\/script>");
+const entryHtmlPath = "./out/index.html";
+const htmlCode = `<!doctype html><html lang="en"><script type="module">${safeJs}</script></html>`;
+await writeFile(entryHtmlPath, htmlCode);
+console.log(`Build complete: ${entryHtmlPath}`);
+
